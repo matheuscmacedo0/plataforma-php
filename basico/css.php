32d131dd9053dc0b4a5e8da5f5dae6b2fd4f542c
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-06-29 15:51:44 -0300

    Criação do css.php

---
+++ basico/css.php
@@ -0,0 +1,17 @@
+<div class="titulo">Integração CSS</div>
+
+<h1>
+
+<?php
+echo 'Olá ';
+echo '<small>';
+echo 'Mundo!';
+echo '</small>';
+?>
+
+</h1>
+
+<?= "<div>Outra forma de exibir!</div>" ?>
+
+<br>
+<div><button><?= "Legal" ?></button></div>
