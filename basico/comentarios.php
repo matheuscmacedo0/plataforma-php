f3d4ae218001a19d9719bc7debb682437d033983
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-06-29 18:08:44 -0300

    Criação do comentarios.php

---
+++ basico/comentarios.php
@@ -0,0 +1,23 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo 'Estou no PHP';
+// Comentário
+## Comentário
+## Comentário
+## Comentário
+## Comentário
+?>
+
+<p>Depois do primeiro bloco de comentários</p>
+
+<?php
+/* 
+Linha 1 de comentário
+Linha 2 de comentário
+Linha 3 de comentário
+?>
+*/
+?>
+<p>Depois do segundo bloco de comentários</p>
+
--- index.php
+++ index.php
@@ -32,6 +32,11 @@
                             Integração CSS
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">
+                            Comentários PHP
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
