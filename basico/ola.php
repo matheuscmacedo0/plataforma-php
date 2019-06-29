0a9a6c1fa053203ab28fa11910999b75298813e5
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-06-29 14:18:26 -0300

    First commit

---
+++ basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo 1</h2>
+
+
+<?php
+echo 'Olá ';
+echo "\n";
+echo 'Mundo!'
+?>
+
+<h2>Exemplo 2</h2>
+
+<?="Outra forma de exibir em PHP" ?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                            Ola PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">
+                            Integração HTML
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        MATHEUS MACEDO WEB DEVELOPER © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
