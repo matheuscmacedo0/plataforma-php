ebbfe488eab148b7741410bdbd05c27ff871539b
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-07-06 16:59:08 -0300

    Resolvendo o desafio

--- tipos/desafio_precedencia.php
+++ tipos/desafio_precedencia.php
@@ -4,11 +4,12 @@
 
 echo '<p>Qual valor será impresso?</p>';
 echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3);
+//Resultado: INF
 
 echo '<p>Qual expressão imprime o valor 100?</p>';
-echo 'a)', (1 + 2) * 20 - 15;
-echo 'b)', 4 * 5 ** 2;
-echo 'c)', 2 ** 3 ** 4 / 1e25;
-echo 'd)', 3 + (3 * 8) / 2 - 3;
+echo 'a)', (1 + 2) * 20 - 15;//45
+echo 'b)', 4 * 5 ** 2; //100
+echo 'c)', 2 ** 3 ** 4 / 1e25;//tende a zero
+echo 'd)', 3 + (3 * 8) / 2 - 3;//12
 
 ?>
