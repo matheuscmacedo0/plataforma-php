c9e36ff0d76617354a3fcd8c6f30554379a2e73b
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-07-06 15:51:58 -0300

    Bases numericas

--- basico/int.php
+++ basico/int.php
@@ -9,6 +9,15 @@ var_dump(11);
 echo '<br>';
 
 echo PHP_INT_MAX;
+echo '<br>';
+echo PHP_INT_MIN;
+echo '<br>';
+echo 017; //base octal
+echo '<br>';
+echo 011101001; //base binaria
+echo '<br>';
+echo 0xf1202; //base hexadecimal
+
 
 
 ?>
