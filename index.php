ddabc231912be4a320e491b83747c26c3ca0cde5
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-07-16 20:58:46 -0300

    Criação do desafio_equacao.php

--- index.php
+++ index.php
@@ -98,6 +98,11 @@
                             Variáveis
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_equacao">
+                            Desafio Equação
+                            </a>
+                        </li>
                         
                     </ul>
                 
