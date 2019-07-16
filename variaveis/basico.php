1e10f3310f56171efb5776d293e0812de757675a
Matheus Macedo <matheuscmacedo@hotmail.com>
2019-07-16 20:56:28 -0300

    Construção do basico.php

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -2,6 +2,39 @@
 
 <?php
 
+$numeroA = 13;
+echo $numeroA, '<br>';
 
+echo '<br>';
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+echo $soma;
+
+echo '<br>';
+
+echo isset($soma);
+
+unset($soma);
+echo '<br>';
+var_dump($soma);
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora eu sou uma string";
+echo '<br>' . $variavel;
+
+//Nomes de variáveis
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vár5 = 'valida'; //evitar acentos
+// $6var = 'invalida';
+// $%7var = 'invalida';
+// $var8% = 'invalida';
+
+echo '<br>';
 
 ?>
